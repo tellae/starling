bde8b7b4e2585479d0d0c11ecc8463aed45b303c
leo-desbureaux-tellae <ldesbureaux@tellae.fr>
2021-03-10 11:36:38 +0100

    doc: fixed conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -15,20 +15,18 @@
 import importlib
 import os
 import sys
-import starling_sim
 
 sys.path.insert(0, os.path.abspath('../'))
-version_module = importlib.import_module("starling_sim.version")
 
 
 # -- Project information -----------------------------------------------------
 
 project = u'Starling'
-copyright = u'2021, Tellae'
+copyright = u'2020-2021, Tellae'
 author = u'Tellae'
 
-# Get the version from the git tag
-# version = (os.popen('git describe').read().strip())[0:6]
+# Get the version from the version module
+version_module = importlib.import_module("starling_sim.version")
 version = '.'.join(version_module.__version__.split('.')[0:2])
 
 # The full version, including alpha/beta/rc tags
