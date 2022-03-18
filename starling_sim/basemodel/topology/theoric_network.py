12cbabefd9d8ebe4413e15a753321388bcefa4a4
leo-desbureaux-tellae <ldesbureaux@tellae.fr>
2022-03-18 15:25:31 +0100

    feat: fetch shortest path in edges instead of computing it (ocmplete graph)

--- starling_sim/basemodel/topology/theoric_network.py
+++ starling_sim/basemodel/topology/theoric_network.py
@@ -1,9 +1,10 @@
 import os
-import networkx as ntx
+import networkx as nx
 import numpy as np
 import scipy.io as sio
 
 from starling_sim.basemodel.topology.topology import Topology
+
 
 class theoricNetwork(Topology):
     """
@@ -16,8 +17,8 @@ class theoricNetwork(Topology):
 
     def init_graph(self):
         nb_stations = len(self.geography["C"])
-        self.graph = ntx.complete_graph(nb_stations, ntx.MultiDiGraph())
-        self.graph.add_edges_from([(i,i) for i in range(nb_stations)])
+        self.graph = nx.complete_graph(nb_stations, nx.MultiDiGraph())
+        self.graph.add_edges_from([(i, i) for i in range(nb_stations)])
         time = "WalkingTime" if self.mode == "walk" else "RidingTime"
         for idx, w in np.ndenumerate(self.geography[time]):
             val = int(w*60*30)
@@ -32,8 +33,42 @@ class theoricNetwork(Topology):
     def position_localisation(self, position):
         return [0,0]
 
-    def nearest_position(self, loc):
+    def nearest_position(self, localisation):
         return [0]
 
-    def localisations_nearest_nodes(self, x, y, method="Null"):
+    def localisations_nearest_nodes(self, x_coordinates, y_coordinates, method=None):
         return [0]
+
+    def dijkstra_shortest_path_and_length(self, origin, destination, parameters, return_weight=False):
+        """
+        Overriding from Topology
+        :param origin: origin position
+        :param destination: destination position
+        :param parameters: parameters defining the utility
+        :param return_weight: also return the total weight
+
+        :return: path (list of positions), duration, length
+        """
+
+        if origin is None or destination is None:
+            raise ValueError("Cannot evaluate path, origin or destination is None")
+
+        # evaluate the weight parameters
+        parameters = {}
+
+        for default in self.weight.default_parameters:
+            if default not in parameters:
+                parameters[default] = self.weight.default_parameters[default]
+
+        self.shortest_path_count += 1
+
+        total_weight, path = self.get_edge_data(origin, destination, "time"), [origin, destination]
+        duration, length = self.evaluate_path_duration_and_length(path)
+
+        if return_weight:
+            return path, duration, length, total_weight
+        return path, duration, length
+
+    def compute_dijkstra_path(self, origin, destination, weight):
+        length, path = self.get_edge_data(origin, destination, "length"), [origin, destination]
+        return path, length
