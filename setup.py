737b5b6a7114d767d2f183ba4618df42443148f5
leo-desbureaux-tellae <73165148+leo-desbureaux-tellae@users.noreply.github.com>
2022-03-21 16:53:06 +0100

    docs: pipy description (#44)

--- setup.py
+++ setup.py
@@ -14,7 +14,31 @@ DESC = "Agent-based framework for mobility simulation"
 # get long description from README.md
 # with open("README.md", "r") as fh:
 #     LONG_DESC = fh.read()
-LONG_DESC = "Long description of the Starling project (TODO)"
+LONG_DESC = r"""
+**Starling** is an agent-based framework for mobility simulation implemented in Python. 
+The framework provides users with tools to quickly develop agent-based simulators of a specific transport service.
+Among these tools, there is a number of generic transport classes that can be used in several transport contexts,
+accessible input and output formats (JSON, GeoJSON) with JSON schema specifications, visualisation and KPI outputs, etc.
+
+**Starling** also comes with three simulation models :
+
+ - A free-floating vehicle-sharing service
+ - A station-based vehicle-sharing service
+ - A station-based vehicle-sharing service with repositioning
+
+**For now, Starling is not adapted to be used as a package : users should rather clone the project 
+locally to develop and run new simulations.**
+
+Read the `docs`_ or see the project on `Github`_.
+
+Citation info: Leblond, V, Desbureaux, L, Bielecki, V., 2020. `A new agent-based software for designing and 
+optimizing emerging mobility services : application to city of Rennes`_. 
+*Proceedings of the European Transport Conference*, 11 September 2020, Milan.
+
+.. _Github: https://github.com/tellae/starling
+.. _docs: https://starling.readthedocs.io/en/latest/
+.. _A new agent-based software for designing and optimizing emerging mobility services \: application to city of Rennes : https://aetransport.org/past-etc-papers/conference-papers-2020?abstractId=6706&state=b
+"""
 
 # list of classifiers from the PyPI classifiers trove
 CLASSIFIERS = [
