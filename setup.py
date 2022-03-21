a9aa488d0bd4001c9fa630656feac1fa5c0f9f56
leo-desbureaux-tellae <ldesbureaux@tellae.fr>
2022-03-21 12:53:47 +0100

    feat: added long description to Starling's PiPy page

--- setup.py
+++ setup.py
@@ -15,19 +15,29 @@ DESC = "Agent-based framework for mobility simulation"
 # with open("README.md", "r") as fh:
 #     LONG_DESC = fh.read()
 LONG_DESC = r"""
-**Starling** is an agent-based framework for mobility simulation. Users can download and model walkable, drivable, or
-bikeable urban networks with a single line of Python code, and then easily
-analyze and visualize them. You can just as easily download and work with
-amenities/points of interest, building footprints, elevation data, street
-bearings/orientations, speed/travel time, and network routing.
-Citation info: Boeing, G. 2017. `OSMnx: New Methods for Acquiring,
-Constructing, Analyzing, and Visualizing Complex Street Networks`_.
-*Computers, Environment and Urban Systems* 65, 126-139.
-doi:10.1016/j.compenvurbsys.2017.05.004
-Read the `docs`_ or see usage examples and demos on `GitHub`_.
-.. _GitHub: https://github.com/gboeing/osmnx-examples
-.. _docs: https://osmnx.readthedocs.io
-.. _OSMnx\: New Methods for Acquiring, Constructing, Analyzing, and Visualizing Complex Street Networks: http://geoffboeing.com/publications/osmnx-complex-street-networks/
+**Starling** is an agent-based framework for mobility simulation implemented in Python. 
+The framework provides users with tools to quickly develop agent-based simulators of a specific transport service.
+Among these tools, there is a number of generic transport classes that can be used in several transport contexts,
+accessible input and output formats (JSON, GeoJSON) with JSON schema specifications, visualisation and KPI outputs, etc.
+
+**Starling** also comes with three simulation models :
+
+ - A free-floating vehicle-sharing service
+ - A station-based vehicle-sharing service
+ - A station-based vehicle-sharing service with repositioning
+
+**For now, Starling is not adapted to be used as a package : users should rather clone the project 
+locally to develop and run new simulations.**
+
+Read the `docs`_ or see the project on `Github`_.
+
+Citation info: Leblond, V, Desbureaux, L, Bielecki, V., 2020. `A new agent-based software for designing and 
+optimizing emerging mobility services : application to city of Rennes`_. 
+*Proceedings of the European Transport Conference*, 11 September 2020, Milan.
+
+.. _Github: https://github.com/tellae/starling
+.. _docs: https://starling.readthedocs.io/en/latest/
+.. _A new agent-based software for designing and optimizing emerging mobility services \: application to city of Rennes : https://aetransport.org/past-etc-papers/conference-papers-2020?abstractId=6706&state=b
 """
 
 # list of classifiers from the PyPI classifiers trove
