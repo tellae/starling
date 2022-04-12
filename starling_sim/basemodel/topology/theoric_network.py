4db12f612718f0d52be6fa9773bfd916d492de7a
leo-desbureaux-tellae <ldesbureaux@tellae.fr>
2022-04-12 14:59:20 +0200

    removed symmetric graph init

--- starling_sim/basemodel/topology/theoric_network.py
+++ starling_sim/basemodel/topology/theoric_network.py
@@ -12,7 +12,7 @@ class theoricNetwork(Topology):
 
     def __init__(self, transport_mode, geography_file, weight_class=None, store_paths=False):
         super().__init__(transport_mode, weight_class=weight_class, store_paths=store_paths)
-        self.geography_file = os.path.join("data/environment/osm_graphs",geography_file)
+        self.geography_file = os.path.join("data/environment/osm_graphs", geography_file)
         self.geography = sio.loadmat(self.geography_file)
 
     def init_graph(self):
@@ -24,8 +24,7 @@ class theoricNetwork(Topology):
             val = int(w*60*30)
             self.graph.edges[idx[0], idx[1], 0]["time"] = val
             self.graph.edges[idx[0], idx[1], 0]["length"] = val
-            self.graph.edges[idx[1], idx[0], 0]["time"] = val
-            self.graph.edges[idx[1], idx[0], 0]["length"] = val
+
 
     def add_time_and_length(self, u, v, d):
         pass
