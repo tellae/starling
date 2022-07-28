c3f15d787bbd5405c3a111e1ef201d6e252a736e
leo-desbureaux-tellae <ldesbureaux@tellae.fr>
2022-07-28 10:27:35 +0200

    new SimulationScenario class for replacing SimulationParameters

--- starling_sim/basemodel/parameters/simulation_parameters.py
+++ starling_sim/basemodel/parameters/simulation_parameters.py
@@ -4,10 +4,118 @@ This module manages the parameters of the simulation
 
 import logging
 import os
+import json
 from copy import deepcopy
 
 from starling_sim.utils.utils import json_load, validate_against_schema
 from starling_sim.utils import paths
+
+
+class SimulationScenario:
+
+    BASE_PARAM_SCHEMA = "parameters.schema.json"
+
+    def __init__(self, scenario_folder_path):
+
+        self.scenario_folder = None
+        self.inputs_folder = None
+        self.outputs_folder = None
+
+        self.parameters = None
+
+        self.model = None
+        self.name = None
+
+        self._get_scenario_folders(scenario_folder_path)
+        self._get_scenario_parameters()
+
+    def _get_scenario_folders(self, scenario_folder_path):
+        # check scenario folder
+        self.scenario_folder = scenario_folder_path
+        if not self.scenario_folder.endswith("/"):
+            self.scenario_folder = self.scenario_folder + "/"
+        if not os.path.exists(self.scenario_folder):
+            raise ValueError("The scenario folder does not exist : {}".format(self.scenario_folder))
+
+        # check inputs folder
+        self.inputs_folder = self.scenario_folder + paths.INPUT_FOLDER_NAME + "/"
+        if not os.path.exists(self.inputs_folder):
+            raise ValueError("The inputs folder does not exist : {}".format(self.inputs_folder))
+
+        # create outputs folder if it does not exist
+        self.outputs_folder = self.scenario_folder + paths.OUTPUT_FOLDER_NAME + "/"
+        if not os.path.exists(self.outputs_folder):
+            os.mkdir(self.outputs_folder)
+
+    def _get_scenario_parameters(self):
+
+        try:
+            parameters_path = self.inputs_folder + paths.PARAMETERS_FILENAME
+            self.parameters = json_load(parameters_path)
+
+        except (FileNotFoundError, json.JSONDecodeError) as e:
+            raise ValueError("Error while loading the scenario parameters : {}".format(str(e)))
+
+        # parameters validation
+        schema = json_load(paths.schemas_folder() + self.BASE_PARAM_SCHEMA)
+        validate_against_schema(self.parameters, schema)
+
+        # change date format from YYYY-MM-DD to YYYYMMDD
+        if "date" in self.parameters:
+            self.parameters["date"] = self.parameters["date"].replace("-", "")
+
+        # get model and scenario
+        self.model = self.parameters["code"]
+        self.name = self.parameters["scenario"]
+
+    def __getitem__(self, item):
+        """
+        Method called when using 'simulationParameters[item]'
+        :param item: Name of the parameter accessed
+        :return: self.parameters[item]
+        """
+
+        if item not in self.parameters:
+            logging.error(
+                "Trying to access unknown parameter '"
+                + str(item)
+                + "'. Does it appears in the parameter entry ?"
+            )
+            raise KeyError("The parameter '" + str(item) + "' does not exist")
+
+        return self.parameters[item]
+
+    def __setitem__(self, key, value):
+        """
+        Method called when using 'simulationParameters[key] = value'
+        :param key: Name of the parameter
+        :param value: Value associated to the parameter
+        :return:
+        """
+
+        if key in self.parameters:
+            logging.warning("Replacing already existing parameter '" + str(key) + "'")
+
+        self.parameters[key] = value
+
+    def __contains__(self, item):
+        """
+        Method called when using 'item in simulationParameters'
+
+        :param item:
+        :return: True if item is in self.parameters, False otherwise
+        """
+
+        return item in self.parameters
+
+    def copy_dict(self):
+        """
+        Return a deepcopy of the simulation parameters.
+
+        :return: deepcopy of simulation parameters
+        """
+
+        return deepcopy(self.parameters)
 
 
 class SimulationParameters:
