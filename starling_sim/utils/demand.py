40c6b74cc7afcbddedaadbd1601f6dd02ea84e33
leo-desbureaux-tellae <ldesbureaux@tellae.fr>
2024-11-04 15:27:45 +0100

    feat: add a demand module

---
+++ starling_sim/utils/demand.py
@@ -0,0 +1,61 @@
+"""
+This module contains utils for generating and managing Starling demand.
+"""
+from os import popen
+
+import pandas as pd
+import geopandas as gpd
+
+def demand_from_eqasim(eqasim_population: gpd.GeoDataFrame) -> gpd.GeoDataFrame:
+    """
+    Generate a Starling population from an Eqasim population.
+
+    :param eqasim_population: GeoDataFrame describing an Eqasim population
+    :return: GeoDataFrame of a Starling population generated from the Eqasim population
+    """
+
+    starling_population = eqasim_population.copy()
+
+    # eqasim population are in epsg:2154, convert to epsg:4326
+    starling_population.to_crs("epsg:4326", inplace=True)
+
+    # eqasim population departure time can be greater than 3600 * 24
+    starling_population["origin_time"] = starling_population["departure_time"] % (3600 * 24)
+    starling_population["origin_time"] = starling_population["origin_time"].astype(int)
+
+    # add attributes specific to Starling demand
+    add_starling_demand_attributes(starling_population)
+
+    return starling_population
+
+
+def default_agent_ids(population: pd.DataFrame) -> list:
+    """
+    Default fonction for generating agent ids on a population.
+
+    :param population: population DataFrame on which agent ids are generated
+    :return: list of agent ids
+    """
+    return ["u-" + str(i) for i in range(len(population))]
+
+
+
+def add_starling_demand_attributes(population: pd.DataFrame, agent_id_generator: callable = default_agent_ids) -> pd.DataFrame:
+    """
+    Add default Starling users attributes: agent type, icon, agent id and mode.
+
+    Caution: this function modifies the original DataFrame.
+
+    :param population: population DataFrame to enrich
+    :param agent_id_generator: function that takes the population as parameter and returns an "agent_id" column
+
+    :return: enriched population DataFrame
+    """
+
+    population["agent_type"] = "user"
+    population["icon"] = "user"
+    population["agent_id"] = agent_id_generator(population)
+    population["mode"] = "walk"
+
+    return population
+
