e0490b2e496e05c3f59604d924ab4fa285ba7917
leo-desbureaux-tellae <ldesbureaux@tellae.fr>
2024-12-09 13:50:59 +0100

    fix: remove unused files

--- .versionrc.js
+++
@@ -1,9 +0,0 @@
-const tracker = {
-    filename: 'starling_sim/__init__.py',
-    updater: '.versionrc.python.updater.js'
-  }
-  
-  module.exports = {
-    bumpFiles: [tracker],
-    packageFiles: [tracker]
-  }
