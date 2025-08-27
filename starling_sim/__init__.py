5c184875287dd62b16de5fba389b2db2687bc95f
leo-desbureaux-tellae <ldesbureaux@tellae.fr>
2025-08-27 13:15:18 +0200

    remove icons on custom levels

--- starling_sim/__init__.py
+++ starling_sim/__init__.py
@@ -5,10 +5,10 @@ from starling_sim.utils.simulation_logging import AGENT_LEVEL, ALGO_LEVEL
 from loguru import logger
 
 # add a log level for agents
-logger.level(AGENT_LEVEL, no=15, color="<blue>", icon="🐍")
+logger.level(AGENT_LEVEL, no=15, color="<blue>")
 
 # add a log level for algorithms
-logger.level(ALGO_LEVEL, no=13, color="<cyan>", icon="🐍")
+logger.level(ALGO_LEVEL, no=13, color="<cyan>")
 
 # disable loguru library wide (call configure_logger or use your own configuration to enable loguru)
 logger.disable("starling_sim")
